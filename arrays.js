383e458b8497d8f485f2a4c71eea019e61b0cc0f
katierose9 <kwhitney98@gmail.com>
2018-01-16 18:02:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,6 @@ function addElementToBeginningOfArray( array, element ) {
 }
 
 function destructivelyAddElementToBeginningOfArray( array, element ) {
-  return [array, element...]
+  array.unshift(element)
+  return array
 }
