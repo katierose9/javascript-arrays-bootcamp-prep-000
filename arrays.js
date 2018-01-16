8ebd8ae6b996fa1875d383752da8ffe72c01d2d6
katierose9 <kwhitney98@gmail.com>
2018-01-16 18:58:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  array.slice(0,
+  array.slice()
+  return array.slice(0,
   array.length - 1)
-  return array
 }
