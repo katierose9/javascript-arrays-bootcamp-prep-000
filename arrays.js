8f3ed2e89a427feca14b04e1fc4acd1c93c8d6b7
katierose9 <kwhitney98@gmail.com>
2018-01-16 18:18:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,8 @@ function destructivelyAddElementToEndOfArray( array, element ) {
 function accessElementInArray( array, index) {
   return array[2]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift
+  return array 
+}
